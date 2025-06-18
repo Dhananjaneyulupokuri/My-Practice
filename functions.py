70d9fd0fdfdbc0a5899b220f8911b541c5474621
P. Dhananjaneyulu <100196147+Dhananjaneyulupokuri@users.noreply.github.com>
2025-06-18 12:01:52 +0530

    Add files via upload

--- functions.py
+++ functions.py
@@ -7,3 +7,24 @@ SampleFunction()
 SampleFunction()
 SampleFunction()
 
+def is_stronger_password(password):
+    if(len(password) < 8):
+       return False
+    if not any (char.isdigit() for char in password):
+         return False   
+    if not any (char.islower() for char in password):
+         return False
+    if not any (char.isupper() for char in password):
+         return False
+    if not any(char in '@#$%^&+=-_' for char in password):
+         return False
+    return True
+print(is_stronger_password("Password123!"))
+print(is_stronger_password("weakpass"))
+
+def factorial(n):
+    if n==0:
+       return 1
+    else:
+       return n*factorial(n-1)
+print(factorial(5)) 
